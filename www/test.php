4c8f449b95c90cb60e077f4d0d767fa78607b8a6
opeoluwa alabi <alabiopeoluwa@gmail.com>
2017-03-28 11:27:42 +0100

    exception handling and error handling

--- www/test.php
+++ www/test.php
@@ -2,11 +2,17 @@
 
 define("DBNAME", "bookstore");
 define("DBUSER", "root");
-define("DBPASS", "vagrant")
+define("DBPASS", "vagrant");
 
+try {
+	#prepare a pdo instance
 $pdo = new PDO('mysql: host=localhost; dbname='.DBNAME, DBUSER, DBPASS);
 
+#set verbose error modes
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
-
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
 
 ?>
