3a9625fd1da523ac4a94551a1cb3c2a692beced3
opeoluwa alabi <alabiopeoluwa@gmail.com>
2017-03-29 02:13:31 +0100

    uploading a file and setting file type and size

--- www/test.php
+++ www/test.php
@@ -14,6 +14,52 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 }catch(PDOException $e){
 	echo $e->getMessage();
 }*/
+#max file size
+
+define("MAX_FILE_SIZE", "2097152");
+
+#allowed extension
+$ext = ["image/jpg", "image/jpeg", "image/png"];
+
+if(array_key_exists("save", $_POST)){
+	#to be sure a file was selected
+	$errors = array();
+	if(empty($_FILES["pic"]["name"])){
+		$errors["pic"] = "please choose a file";
+	}
+	#check file size
+	if($_FILES["pic"]["size"] > MAX_FILE_SIZE){
+		$errors["pic"] = "file size exceeds maximum. ".MAX_FILE_SIZE;
+	}
+
+	#check extension
+	if(!in_array($_FILES["pic"]["type"], $ext)){
+		$errors["pic"] = "invalid file type";
+	}
+
+	#generate random number to append
+	$rnd = rand(00000, 99999);
+
+	#strip filename for spaces
+	$strip_name = str_replace((""), "_", $_FILES["pic"]["name"]);
+
+	$filename = $rnd.$strip_name;
+	$destination = "uploads/".$filename;
+
+	if(!move_uploaded_file($_FILES["pic"]["tmp_name"], $destination)){
+		$errors["pic"] = "file upload failed";
+	}
+
+	if(empty($errors)){
+		echo "done";
+	}else {
+		foreach ($errors as $err) {
+		echo $err. "</br>";
+
+		}
+	}
+}
+
 
 ?>
 
