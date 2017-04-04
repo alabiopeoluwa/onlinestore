f019b5c5a8ddf7c6b6078d06627c922db27d238b
opeoluwa alabi <alabiopeoluwa@gmail.com>
2017-04-04 12:43:43 +0100

    creating a function for the login page

--- www/includes/functions.php
+++ www/includes/functions.php
@@ -43,7 +43,33 @@
 	return $result;
 	}
 
+function adminLogin($pdo, $enter){
+	$stmt = $pdo->prepare("SELECT * FROM admin WHERE email=:e");
 
+	#bind params
+	$stmt->bindParam(":e", $enter["email"]);
+	$stmt->execute();
+
+	$count = $stmt->rowCount();
+
+	if($count==1){
+		$row = $stmt->fetch(PDO::FETCH_ASS0C);
+
+		if(password_verify($enter["password"], $row["hash"])){
+
+			$_SESSION["id"] = $row["admin"];
+			$_SESSION["email"] = &row["email"];
+
+			header("Location:home.php");
+		}
+		else{
+
+			$login_error = "wrong email or password";
+			header("Location:login.php?login_error=$login_error");
+		}
+	}
+
+}
 
 
 
